d266da81946c2c93ecdec07c8f961a8e964225bb
Fernando Benavides <nockzblack@gmail.com>
2022-04-04 10:18:41 -0500

    Initial Commit

---
+++ Wrappers/WrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WrappersApp.swift
+//  Wrappers
+//
+//  Created by Fernando's Mac on 04/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
